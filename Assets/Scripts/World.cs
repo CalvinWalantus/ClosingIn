508a2b251fd3d3a99477c516ee2a268678a3c7d2
Calvin Gill Walantus <cwalantu@crm.ucsc.edu>
2018-01-20 15:51:33 -0800

    get updated world.cs from dev

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -6,12 +6,23 @@ public class World : MonoBehaviour {
 
 	// True = 3D
 	// False = 2D
-	bool dimension = false; 
+	bool dimension = false;
+
+	int two_shot;
+	int three_shot;
+
 	float shift_time = 5;
 	public float timer = 0;
 
+	// Event signaling a dimension change (a "shift")
 	public delegate void Shift(bool dim, float time);
 	public event Shift shiftEvent;
+
+	// Event signalling a shot change
+	// 1-4 = 2D shot change
+	// 5-8 = 3D shot change
+	public delegate void ShotChange (int shot);
+	public event ShotChange shotChangeEvent;
 
 	void Start() {
 		timer = shift_time;
@@ -24,6 +35,23 @@ public class World : MonoBehaviour {
 			timer = 0;
 		}
 		timer += Time.deltaTime;
+
+
+		Event e = Event.current;
+		if (e.type == EventType.KeyDown && char.IsDigit (e.character)) {
+			int shot = (int)e.character;
+			if (shot > 0 && shot < 9) {
+				shotChangeEvent (shot);
+				if (shot < 5) {
+					two_shot = shot;
+				} else {
+					three_shot = shot - 4;
+				}
+			}
+		}
+
+
+
 	}
 
 }
