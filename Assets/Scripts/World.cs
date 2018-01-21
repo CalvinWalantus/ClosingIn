2d8d4d231b854809e4e7d8fc5f8cb6ca54df33cf
Calvin Gill Walantus <cwalantu@crm.ucsc.edu>
2018-01-20 19:51:25 -0800

    almost finished cinemachine

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -6,12 +6,12 @@ public class World : MonoBehaviour {
 
 	// True = 3D
 	// False = 2D
-	bool dimension = false;
+	public bool dimension = false;
 
-	int two_shot;
-	int three_shot;
+	public int two_shot = 1;
+	public int three_shot = 1;
 
-	float shift_time = 5;
+	public float shift_time = 5;
 	public float timer = 0;
 
 	// Event signaling a dimension change (a "shift")
@@ -21,35 +21,37 @@ public class World : MonoBehaviour {
 	// Event signalling a shot change
 	// 1-4 = 2D shot change
 	// 5-8 = 3D shot change
-	public delegate void ShotChange (int shot);
+	public delegate void ShotChange (int tw_shot, int th_shot);
 	public event ShotChange shotChangeEvent;
 
 	void Start() {
 		timer = shift_time;
+		shotChangeEvent (two_shot, three_shot);
+		shiftEvent (dimension, shift_time);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftShift) && timer > shift_time) {
+		// Check if user has pressed shift to bring about a dimension shift
+		if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) && timer > shift_time) {
+			dimension = !dimension;
 			shiftEvent(dimension, shift_time);
 			timer = 0;
 		}
 		timer += Time.deltaTime;
 
-
-		Event e = Event.current;
-		if (e.type == EventType.KeyDown && char.IsDigit (e.character)) {
-			int shot = (int)e.character;
-			if (shot > 0 && shot < 9) {
-				shotChangeEvent (shot);
+		// Check if user has pressed the 1-8 keys to bring about a shot change.
+		for (int shot = 1; shot < 9; shot++) {
+			if (Input.GetKeyDown((KeyCode)shot+48)) {
 				if (shot < 5) {
 					two_shot = shot;
 				} else {
 					three_shot = shot - 4;
 				}
+				shotChangeEvent (two_shot, three_shot);
+				break;
 			}
 		}
-
 
 
 	}
