eff3c6302041aed621ceff2f44c676c8b69cedbb
Calvin Gill Walantus <cwalantu@crm.ucsc.edu>
2018-02-04 14:59:40 -0800

    add Collapsabel character prefab

--- Assets/Scripts/Platforms_andInteractables/ComplexCompressable.cs
+++ Assets/Scripts/Platforms_andInteractables/ComplexCompressable.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ComplexCompressable : MonoBehaviour {
 
+	public Component script;
+
 	// Use this for initialization
 	public virtual void ComplexCompress (int two_shot, Vector3 player_position) {
-		
+		if (script != null) {
+			//script.ComplexCompress();
+		}
 	}
 
 	public virtual void ComplexDecompress () {
