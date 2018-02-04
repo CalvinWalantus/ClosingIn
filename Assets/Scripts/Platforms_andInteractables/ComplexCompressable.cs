046d73feed93031e689179698d2db06707b20802
Calvin Walantus <cwalantu@ucsc.edu>
2018-02-04 03:26:12 -0800

    debugging SOI

---
+++ Assets/Scripts/Platforms_andInteractables/ComplexCompressable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComplexCompressable : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void ComplexCompress (int two_shot, Vector3 player_position) {
+		
+	}
+
+	public virtual void ComplexDecompress () {
+
+	}
+
+}
