2ecb154de90c16a1a5412cbacdb0103e2ff6fce3
Calvin Gill Walantus <cwalantu@crm.ucsc.edu>
2018-02-13 17:54:27 -0800

    working on playtest world

--- Assets/Scripts/Platforms_andInteractables/Teleport.cs
+++ Assets/Scripts/Platforms_andInteractables/Teleport.cs
@@ -6,10 +6,30 @@ public class Teleport : MonoBehaviour {
 
 	public Transform destination;
 
+	public World worldController;
+
+	public bool switchDimension;
+	public bool dimensionToChangeTo;
+	public int twoShot, threeShot;
+
+	public delegate void Respawn(bool dim, int two_shot, int three_shot);
+	public event Respawn RespawnEvent;
+
+	void Start () {
+		worldController = FindObjectOfType<World>();
+	}
+
 	void OnTriggerEnter (Collider other) {
 		if (other.CompareTag("Player")) {
+			
 			other.transform.position = destination.position;
 			other.transform.rotation = destination.rotation;
+
+			if (switchDimension) {
+				
+				RespawnEvent (dimensionToChangeTo, twoShot, threeShot);
+
+			}
 		}
 	}
 }
