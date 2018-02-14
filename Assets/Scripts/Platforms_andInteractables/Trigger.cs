aab3af52ceb3a375db4c9c8dc093e491d58f0c7a
Calvin Gill Walantus <cwalantu@crm.ucsc.edu>
2018-02-14 10:20:53 -0800

    basic ortho switch

--- Assets/Scripts/Platforms_andInteractables/Trigger.cs
+++ Assets/Scripts/Platforms_andInteractables/Trigger.cs
@@ -2,35 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor.SceneManagement;
-#endif
+public class Trigger : MonoBehaviour {
 
-public class Trigger : MonoBehaviour 
-{
 	public int id = 0;
 
 	public delegate void Hit(int trigger_id);
 	public event Hit hitEvent;
 
-	public string name;
-
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 
 	}
-
+	
 	// Update is called once per frame
-	void OnTriggerEnter (Collider other) 
-	{
-		if (other.tag.Equals("Player")) 
-		{
-			//hitEvent (id);
-			Application.LoadLevel(name);
-			#if UNITY_EDITOR
-			EditorSceneManager.LoadScene(name);
-			#endif
+	void OnTriggerEnter (Collider other) {
+		if (other.tag.Equals("Player")) {
+			hitEvent (id);
 		}
 	}
 }
