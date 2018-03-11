085908bb712fea0e6a72cb09fde92567fc1fd4cb
Christopher Paz <cpaz1@ucsc.edu>
2018-03-10 20:56:18 -0800

    Feedback of Switch Trigger

--- Assets/Scripts/Platforms_andInteractables/Trigger.cs
+++ Assets/Scripts/Platforms_andInteractables/Trigger.cs
@@ -6,18 +6,40 @@ public class Trigger : MonoBehaviour {
 
 	public int id = 0;
 
+	Color start_color;
+	Material trigger_switch;
+	bool once_only = true;
+
 	public delegate void Hit(int trigger_id);
 	public event Hit hitEvent;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		trigger_switch = gameObject.GetComponent<MeshRenderer>().material;
+		start_color = trigger_switch.GetColor("_Color");
 
+		Debug.Log(start_color.ToString());
 	}
 	
 	// Update is called once per frame
-	void OnTriggerEnter (Collider other) {
-		if (other.tag.Equals("Player")) {
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.tag.Equals("Player") && once_only) 
+		{
 			hitEvent (id);
+			StartCoroutine(FeedbackLerp(start_color, Color.gray, 3));
 		}
+	}
+
+	private IEnumerator FeedbackLerp(Color src, Color dst, float duration)
+	{
+		float startTime = Time.time;
+
+		while (Time.time - startTime < duration) 
+		{
+			trigger_switch.SetColor("_Color", Color.Lerp (src, dst, (Time.time - startTime) / duration));
+		}
+		yield return 1;
 	}
 }
