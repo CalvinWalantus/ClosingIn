5881f64dbb7bbf8445edfedf7dbf1cab03487e04
Calvin Gill Walantus <cwalantu@crm.ucsc.edu>
2018-03-15 15:39:02 -0700

    switch feedback working

--- Assets/Scripts/Platforms_andInteractables/Trigger.cs
+++ Assets/Scripts/Platforms_andInteractables/Trigger.cs
@@ -6,8 +6,13 @@ public class Trigger : MonoBehaviour {
 
 	public int id = 0;
 
-	Color start_color;
-	Material trigger_switch;
+	Material start_material;
+	public Material end_material;
+
+	public float lerpSpeed = 100.0f;
+
+	MeshRenderer rend;
+
 	bool once_only = true;
 
 	public delegate void Hit(int trigger_id);
@@ -16,8 +21,8 @@ public class Trigger : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		trigger_switch = gameObject.GetComponent<MeshRenderer>().material;
-		start_color = trigger_switch.GetColor("_EmissionColor");
+		rend = GetComponent<MeshRenderer> ();
+		start_material = rend.material;
 	}
 	
 	// Update is called once per frame
@@ -26,21 +31,21 @@ public class Trigger : MonoBehaviour {
 		if (other.tag.Equals("Player") && once_only) 
 		{
 			hitEvent (id);
-			StartCoroutine(FeedbackLerp(start_color, Color.grey, 3));
-			trigger_switch.SetColor ("_EmissionColor", Color.gray);
+			StartCoroutine(FeedbackLerp(start_material, end_material, lerpSpeed));
+
 
 			GetComponent<AudioSource>().PlayDelayed(0.5f);
 			once_only = false;
 		}
 	}
 
-	private IEnumerator FeedbackLerp(Color src, Color dst, float duration)
+	private IEnumerator FeedbackLerp(Material src, Material dst, float duration)
 	{
 		float startTime = Time.time;
 
-		while (Time.time - startTime < duration) 
+		while (Time.time - startTime <= duration) 
 		{
-			trigger_switch.SetColor("_EmissionColor", Color.Lerp(src, dst, (Time.time - startTime) / duration));
+			rend.material.Lerp (src, dst, (Time.time - startTime) / duration);
 			yield return 1;
 		}
 	}
