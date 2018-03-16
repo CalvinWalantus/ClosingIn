0559cc7b9559252a6b2e61d5bce32c3553a918e8
chris zhang <chris34481203@gmail.com>
2018-03-16 16:28:24 -0700

    1
    1

---
+++ Assets/visible.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visible : MonoBehaviour {
+	public List<Renderer> visibleRenderers = new List<Renderer>();
+	public List<Renderer> temp = new List<Renderer>();
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Renderer[] sceneRenderers = FindObjectsOfType<Renderer> ();
+
+		// Store only visible renderers
+		visibleRenderers.Clear ();
+		for (int i = 0; i < sceneRenderers.Length; i++) {
+			if (sceneRenderers [i].isVisible)
+				visibleRenderers.Add (sceneRenderers [i]);
+		}
+		foreach (Renderer i in visibleRenderers) {
+			if (i.gameObject.transform.position.z < player.transform.position.z && i.gameObject.transform.position.z < Camera.main.transform.position.z) {
+				i.GetComponent<Renderer> ().enabled = false;
+				temp.Add (i);
+			}
+		}
+	}
+//	bool leftorright(Transform play, Vector3 target){
+//		//float temp = play.InverseTransformPoint (target);
+//		//if (temp != 0) {
+//		//	return true;
+//		//} else {
+//		//	return false;
+//		//}
+//		return true;
+//	}
+}
