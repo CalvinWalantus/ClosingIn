8b5a42f34a801a5a82df05e2c88fc63d639f554c
Zijie Zhang <zzhang40@crm.ucsc.edu>
2018-03-17 16:59:33 -0700

    foreground

--- Assets/Scripts/foreground.cs
+++ Assets/Scripts/foreground.cs
@@ -5,43 +5,84 @@ using System.Linq;
 
 public class foreground : MonoBehaviour {
 	public GameObject player;
-	private List<GameObject> disables = new List<GameObject>();
+	public List<Collider> disables = new List<Collider>();
 	public List<Collider> temp = new List<Collider> ();
+	public Collider[] hits;
 	World world_controller;
+	public int layermask;
 	// Use this for initialization
 	void Start () {
 		world_controller = FindObjectOfType<World> ();
-		
+		layermask = 1 << 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (world_controller.dimension == false) {
 			findobjects ();
+		} else {
+			foreach (Collider i in disables.ToList()) {
+				i.GetComponent<Renderer> ().enabled = true;
+			}
 		}
 	}
 
 	void findobjects(){
+		temp.Clear ();
 		float x = (player.transform.position.x + Camera.main.transform.position.x)/2;
 		float y = (player.transform.position.y + Camera.main.transform.position.y)/2;
 		float z = (player.transform.position.z + Camera.main.transform.position.z)/2;
 		Vector3 pos = new Vector3 (x, y, z);
 		float size = Vector3.Distance (player.transform.position, Camera.main.transform.position);
 		size = size * 0.9f;
-		pos = new Vector3 (pos.x + 10, pos.y, pos.z);
-		Collider[] hits = Physics.OverlapBox (pos, new Vector3 (20f/2, 8.3f/2, size/3));
-		Debug.Log (hits.Length);
-		//List<Collider> temp = hits.ToList ();
-		foreach(GameObject i in disables){
-			if(temp.Contains(i.GetComponent<Collider>()))
-				i.GetComponent<Renderer> ().enabled = true;
-		}
-		foreach (Collider i in hits) {
-			if (i.gameObject.GetComponent<Renderer> ()) {
-				i.gameObject.GetComponent<Renderer> ().enabled = false;
-				disables.Add (i.gameObject);
+		hits = Physics.OverlapBox (pos, new Vector3 (20f/2, 8.3f/2, size/2),Quaternion.identity,layermask);
+		//debugdraw (hits);
+		temp = hits.ToList();
+		foreach (Collider q in disables.ToList()) {
+			if (!temp.Contains (q)) {
+				q.GetComponent<Renderer> ().enabled = true;
+				disables.Remove (q);
+				//Debug.Log (q.gameObject.name);
 			}
 		}
+		foreach(Collider i in temp){
+			if (world_controller.two_shot == 3) {
+				if (i.gameObject.transform.position.z - i.gameObject.transform.lossyScale.z / 2 > player.transform.position.z && !disables.Contains(i)) {
+					if (i.GetComponent<Renderer> ()) {
+						i.GetComponent<Renderer> ().enabled = false;
+						disables.Add (i);
+						Debug.Log (i.gameObject.name);
+					}
+				}
+			}
+			if (world_controller.two_shot == 1) {
+				if (i.gameObject.transform.position.z + i.gameObject.transform.lossyScale.z / 2 < player.transform.position.z && !disables.Contains(i)) {
+					if (i.GetComponent<Renderer> ()) {
+						i.GetComponent<Renderer> ().enabled = false;
+						disables.Add (i);
+					}
+				}
+			}
+			if (world_controller.two_shot == 4) {
+				if (i.gameObject.transform.position.x + i.gameObject.transform.lossyScale.z / 2 < player.transform.position.z && !disables.Contains(i)) {
+					if (i.GetComponent<Renderer> ()) {
+						i.GetComponent<Renderer> ().enabled = false;
+						disables.Add (i);
+					}
+				}
+			}
+			if (world_controller.two_shot == 2) {
+				if (i.gameObject.transform.position.z - i.gameObject.transform.lossyScale.z / 2 > player.transform.position.z && !disables.Contains(i)) {
+					if (i.GetComponent<Renderer> ()) {
+						i.GetComponent<Renderer> ().enabled = false;
+						disables.Add (i);
+					}
+				}
+			}
+
+
+		}
+
 	}
 	void OnDrawGizmos() {
 		Gizmos.color = Color.red;
@@ -50,10 +91,13 @@ public class foreground : MonoBehaviour {
 		float z = (player.transform.position.z + Camera.main.transform.position.z)/2;
 		Vector3 pos = new Vector3 (x, y, z);
 		float size = Vector3.Distance (player.transform.position, Camera.main.transform.position);
-		size = size * 0.9f;
-
-		Gizmos.DrawCube(pos,new Vector3(20f,8.3f,size));
-		Debug.Log(Camera.current.pixelRect);
+		Gizmos.DrawWireCube(pos,new Vector3(20f,8.3f,size));
 	}
-
+	void debugdraw(Collider[] hits){
+		foreach (Collider i in hits) {
+			if (i.gameObject.GetComponent<Renderer> ()) {
+				i.GetComponent<Renderer> ().material.color = Color.red;
+			}
+		}
+	}
 }
