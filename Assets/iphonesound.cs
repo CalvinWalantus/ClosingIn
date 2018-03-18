8c7f1cca202a7968a6fb4806faa7a9abec731b51
Zijie Zhang <zzhang40@crm.ucsc.edu>
2018-03-18 08:31:45 -0700

    4

---
+++ Assets/iphonesound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iphonesound : MonoBehaviour {
+	public AudioSource audioplay;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log (collision.gameObject.name);
+		if (collision.gameObject.name == "Caedl1") {
+			audioplay.Play ();
+			audioplay.loop = true;
+			audioplay.spatialBlend = 1f;
+		}
+	}
+}
