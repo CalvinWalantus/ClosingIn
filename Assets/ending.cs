3763f3720cc850c974b49ca81e93e1728a088349
Zijie Zhang <zzhang40@crm.ucsc.edu>
2018-03-18 12:39:36 -0700

    light
    1

--- Assets/ending.cs
+++ Assets/ending.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ending : MonoBehaviour {
-	public GUITexture guiTextre;
+	public Texture2D fadeTexture;
+
+	[Range(0.1f,1f)]
+	public float fadespeed;
+	public int drawDepth = -1000;
+
+	private float alpha = 0.0f;
+	private float fadeDir = -1f;
+	private bool startfade;
 	// Use this for initialization
 	void Start () {
-		
+		startfade = false;
 	}
 	
 	// Update is called once per frame
@@ -16,6 +24,24 @@ public class ending : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		Debug.Log (collision.gameObject.name);
 		if (collision.gameObject.name == "Caedl1") {
+			startfade = true;
+		}
+	}
+	void OnGUI() {
+		if (startfade) {
+			alpha -= fadeDir * fadespeed * Time.deltaTime;
+			alpha = Mathf.Clamp01 (alpha);
+
+			Color newColor = GUI.color; 
+			newColor.a = alpha;
+
+			GUI.color = newColor;
+
+			GUI.depth = drawDepth;
+
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeTexture);
+			string words = "Coming Up on spring quarter";
+			words = GUI.TextField (new Rect (100, 100, 300, 300), words, 50);
 		}
 	}
 
