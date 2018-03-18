2f6966020683123480c2f1629b6b69f3f696fa59
Christopher Paz <cpaz1@ucsc.edu>
2018-03-17 18:25:11 -0700

    walking detection works, sound timing is next

---
+++ Assets/Scripts/Player_andCamera/Footsteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour 
+{
+	ThirdPersonCharacter tpc;
+	Animator anim;
+	AudioSource audio_source;
+
+	// Use this for initialization
+	void Start () 
+	{
+		tpc = GetComponentInParent<ThirdPersonCharacter>();
+		anim = GetComponent<Animator>();
+		audio_source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(tpc.GetGroundStatus() == true && anim.GetCurrentAnimatorStateInfo(0).IsName("Walking") && audio_source.isPlaying == false)
+		{
+			audio_source.PlayDelayed(1f);
+		}
+	}
+}
