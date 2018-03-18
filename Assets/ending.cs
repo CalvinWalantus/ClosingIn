1d0ec68f63fbd9bcbb0353e1a208e407843ee378
Zijie Zhang <zzhang40@crm.ucsc.edu>
2018-03-18 06:41:20 -0700

    1

---
+++ Assets/ending.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ending : MonoBehaviour {
+	public GUITexture guiTextre;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log (collision.gameObject.name);
+		if (collision.gameObject.name == "Caedl1") {
+		}
+	}
+
+}
