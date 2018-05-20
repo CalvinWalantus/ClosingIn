4987b1d4340bd63d3a8d10f1c4a6396c24d4b65d
Christopher Paz <cpaz1@ucsc.edu>
2018-05-19 17:44:56 -0700

    Added new AudioMixer and AudioTrigger prefab and script

---
+++ Assets/Scripts/Menus_Levels/AudioTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger:MonoBehaviour
+{
+	public Dictionary<string, float> channel = new Dictionary<string, float>(); 
+
+
+}
