5ce3d4963c6a7614465b963c1eebad5a2b22f71f
Christopher Paz <cpaz1@ucsc.edu>
2018-05-21 23:22:51 -0700

    Added night skybox. Confused on AudioTrigger

--- Assets/Scripts/Menus_Levels/AudioTrigger.cs
+++ Assets/Scripts/Menus_Levels/AudioTrigger.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioTrigger:MonoBehaviour
 {
-	public Dictionary<string, float> channel = new Dictionary<string, float>(); 
+	public AudioMixer master_mixer;
+	public Dictionary<AudioMixerGroup, float>[] channels;
+
+
 
 
 }
