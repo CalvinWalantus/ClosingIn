466345807b2dbfc02f261470971cb04805aa144b
Christopher Paz <cpaz1@ucsc.edu>
2018-05-22 18:31:31 -0700

    Not much progress. Detects where mouse or camera is.

---
+++ Assets/Scripts/Player_andCamera/HighlightObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    public Camera camera;
+    public Material mat;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(ray);
+
+        if(!(Physics.Raycast(ray, out hit)))
+        {
+            Debug.Log("Nothing");
+        }
+        else
+        {
+            Transform objectHit = hit.transform;
+
+            // Do something with the object that was hit by the raycast.
+            GetComponent<Renderer>().material = mat;
+        }
+    }
+}
