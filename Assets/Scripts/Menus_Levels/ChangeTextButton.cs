f742b107220ce2cdb5fe37106e73ed3da1e56497
Christopher Paz <cpaz1@ucsc.edu>
2018-06-08 21:52:08 -0700

    Show Calvin changes

--- Assets/Scripts/Menus_Levels/ChangeTextButton.cs
+++ Assets/Scripts/Menus_Levels/ChangeTextButton.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeTextButton : MonoBehaviour
 {
-	void OnMouseOver()
+	public Text text;
+
+	void Start()
+	{
+		text = GetComponent<Text>();
+		Debug.Log(text.transform);
+	}
+
+	void OnHover()
 	{
 		Debug.Log("This is Supposed to Change");
 	}
