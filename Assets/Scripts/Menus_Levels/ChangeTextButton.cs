8a1d18ff524dfada3c68cba4c1796742be1df68a
Christopher Paz <cpaz1@ucsc.edu>
2018-06-08 21:34:12 -0700

    New script to change text and deleted extra/old Title

---
+++ Assets/Scripts/Menus_Levels/ChangeTextButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextButton : MonoBehaviour
+{
+	void OnMouseOver()
+	{
+		Debug.Log("This is Supposed to Change");
+	}
+}
